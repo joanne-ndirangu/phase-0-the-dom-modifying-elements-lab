2080f1f5329bcefa8b25a6ad75e6373587162e16
Joanne Ndirangu <joannendirangu15@gmail.com>
2023-07-04 00:25:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+function removeMainNode(){
+    const main = document.querySelector("#main");
+    main.remove();
+}
+removeMainNode();
+
+
+const newHeader = document.createElement("H1");
+newHeader.id = "victory";
+newHeader.textContent = "RON is the champion";
+document.body.append(newHeader);
+
